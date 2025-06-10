c5eabecad8999e8fef08c7108d73eed201bb238c
vkosheleva <v.kosheleva@optisoft.net>
2025-06-10 15:47:20 +0300

    change web-elements

--- js/script41.js
+++ js/script41.js
@@ -17,3 +17,33 @@ const hearts = document.querySelectorAll(".heart");
 hearts.forEach((item) => {
   console.log(item);
 });
+
+box.style.backgroundColor = "blue";
+box.width = "500px";
+btns[1].style.borderRadius = "200%";
+circles[0].style.backgroundColor = "white";
+
+box.style.cssText = "background-color: blue; width: 500px; height: 500px;";
+
+for (let i = 0; i < hearts.length; i++) {
+  hearts[i].style.backgroundColor = "green";
+}
+
+hearts.forEach((item) => {
+  item.style.backgroundColor = "red";
+});
+
+const div = document.createElement("div");
+const text = document.createTextNode("It was I");
+
+div.classList.add("black");
+
+document.body.append(div);
+
+const wrapper = document.querySelector(".wrapper");
+wrapper.append(div);
+wrapper.prepend(div);
+hearts[0].before(div);
+hearts[0].after(div);
+circles[2].remove();
+circles[3].replaceWith(circles[0]);
