b317153d94d55cb69c76a0158aeea2c46d2b3cc1
vkosheleva <v.kosheleva@optisoft.net>
2025-06-16 11:54:31 +0300

    59 task "sum of page number", "pangram"

--- js/script59.js
+++ js/script59.js
@@ -30,3 +30,39 @@ studentsSet.forEach((value, valueAgain, set) => {
 console.log(studentsSet.values());
 console.log(studentsSet.keys());
 console.log(studentsSet.entries());
+
+function amountOfPages(summary) {
+  let numberSum = "";
+  let n = "";
+
+  while (numberSum.length < summary) {
+    n++;
+    numberSum = numberSum + n;
+    // console.log("numberSum = ", numberSum);
+    // console.log("n = ", n);
+  }
+  return n;
+}
+
+console.log(amountOfPages(5));
+console.log(amountOfPages(25));
+console.log(amountOfPages(1095));
+console.log(amountOfPages(185));
+
+function isPangram(string) {
+  if (string.length < 26) return false;
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  string
+    .replace(" ", "")
+    .split()
+    .forEach((lit) => {
+      if (alphabet.includes(lit.toLowerCase() == -1)) {
+        return false;
+      }
+    });
+  return true;
+}
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog"));
+console.log(isPangram("Hello world"));
