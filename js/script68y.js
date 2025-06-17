a3d0cb2c3025da986662b9e456460cd798baa31f
vkosheleva <v.kosheleva@optisoft.net>
2025-06-17 15:39:41 +0300

    68 date

---
+++ js/script68y.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function getTimeRemaining(endtime) {
+  const t = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((t / 1000) % 60);
+  const minutes = Math.floor((t / 1000 / 60) % 60);
+  const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+  //   return {
+  //     total: t,
+  //     days: days,
+  //     hours: hours,
+  //     minutes: minutes,
+  //     seconds: seconds,
+  //   };
+
+  return { total: t, days, hours, minutes, seconds };
+}
+
+console.log(getTimeRemaining("2025-08-30"));
