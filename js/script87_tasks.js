bf0d2ec868181ffc008b604facb1c8069ea211a7
vkosheleva <v.kosheleva@optisoft.net>
2025-06-30 13:54:44 +0300

    tasks for arrays2

--- js/script87_tasks.js
+++ js/script87_tasks.js
@@ -52,3 +52,27 @@ function checkFilms(arr) {
 }
 
 console.log(checkFilms(tranformedArray));
+
+const funds = [
+  { amount: -1400 },
+  { amount: 2400 },
+  { amount: -1000 },
+  { amount: 500 },
+  { amount: 10400 },
+  { amount: -11400 },
+];
+
+const getPositiveIncomeAmount = (data) => {
+  return data
+    .filter((item) => item.amount > 0)
+    .reduce((sum, item) => sum + item.amount, 0);
+};
+console.log(getPositiveIncomeAmount(funds));
+
+const getTotalIncomeAmount = (data) => {
+  const haveNegative = data.some((item) => item.amount < 0);
+  return haveNegative
+    ? data.reduce((sum, item) => sum + item.amount, 0)
+    : getPositiveIncomeAmount(data);
+};
+console.log(getTotalIncomeAmount(funds));
